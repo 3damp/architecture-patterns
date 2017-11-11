3d331ea4f79b67b9a37e48eb2463783ad6e7ea25
Armand <amp206@gmail.com>
2017-11-11 19:40:17 +0000

    gitignore

--- SoftwareArchitecturePracticalExamples/src/p3/rmi/AccountServer.java
+++ SoftwareArchitecturePracticalExamples/src/p3/rmi/AccountServer.java
@@ -5,33 +5,32 @@ import java.rmi.registry.Registry;
 import java.rmi.server.UnicastRemoteObject;
 
 /**
- * Usage run the server then run the client
- * When your finished stop the server
- *  
+ * Usage run the server then run the client When your finished stop the server
+ * 
  * @author Dr 40004938
  */
-public class AccountServer{
+public class AccountServer {
 
-	public  static void main(String args[]) {
-		try { 
-			
-			//Start the registry
-			 Registry registry = LocateRegistry.createRegistry(1099); 
-			
-	        // Instantiate the concrete class 
-	         Account acc = new Account(); 
-	    
-	         // Export the stub using the interface specification
-	         IAccount stub = (IAccount) UnicastRemoteObject.exportObject(acc, 0);  
-	         
-	         //Bind the stub to the name "Account" in the registry 
-	         registry.bind("Account", stub);  
-	         
-	         System.out.println("Server ready");
-	         
-	      } catch (Exception e) { 
-	         System.err.println("Server exception: " + e.toString()); 
-	         e.printStackTrace(); 
-	      } 
+	public static void main(String args[]) {
+		try {
+
+			// Start the registry
+			Registry registry = LocateRegistry.createRegistry(1099);
+
+			// Instantiate the concrete class
+			Account acc = new Account();
+
+			// Export the stub using the interface specification
+			IAccount stub = (IAccount) UnicastRemoteObject.exportObject(acc, 0);
+
+			// Bind the stub to the name "Account" in the registry
+			registry.bind("Account", stub);
+
+			System.out.println("Server ready");
+
+		} catch (Exception e) {
+			System.err.println("Server exception: " + e.toString());
+			e.printStackTrace();
+		}
 	}
 }
